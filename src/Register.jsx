980f31717b4e0b301df77274d49e1cb76537c8bf
Sameer847 <54467266+Sameer847@users.noreply.github.com>
2025-03-08 20:43:59 +0530

    Update Register.jsx

--- src/Register.jsx
+++ src/Register.jsx
@@ -11,7 +11,7 @@ const Register = () => {
 
     const handleRegister = async () => {
         try {
-            await axios.post('http://localhost:5000/register', {
+            await axios.post('https://mern-backend-registration-1.onrender.com/register', {
                 name,
                 email,
                 password,
